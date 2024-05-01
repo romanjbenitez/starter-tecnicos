435d151afbf3dc18a804e438ebc7554b0a6771f3
romanjbenitez <roman200217@gmail.com>
2024-05-01 15:42:55 -0300

    primer commit

---
+++ tecnica/src/test/java/com/prueba/tecnica/TecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.tecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
